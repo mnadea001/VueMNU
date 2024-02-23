d0155531342e047f790fe3798948e3401929d361
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-02-23 15:47:56 +0100

    3D custom OK

--- vue-project/src/views/AnimateView.vue
+++ vue-project/src/views/AnimateView.vue
@@ -5,29 +5,47 @@ import {
   Camera,
   Renderer,
   Scene,
-  LambertMaterial,
-} from 'troisjs';
-import { defineComponent, ref, onMounted } from 'vue';
+  Sphere,
+  BasicMaterial,
+  Texture,
+} from "troisjs";
+import { defineComponent, ref, onMounted, reactive } from "vue";
 
 export default defineComponent({
-  components: { Box, Camera, Renderer, Scene, PointLight, LambertMaterial },
+  components: {
+    Box,
+    Camera,
+    Renderer,
+    Scene,
+    PointLight,
+    Sphere,
+    BasicMaterial,
+    Texture,
+  },
   setup() {
     const renderer = ref(null);
     const box = ref(null);
+    const sphere = ref(null);
 
     onMounted(() => {
       renderer?.value?.onBeforeRender(() => {
         box.value.mesh.rotation.x += 0.01;
-    }); 
-    })
+        sphere.value.mesh.rotation.y += 0.001;
+      });
+    });
+    const spherePosition = reactive({
+      spherePosX: -10,
+      spherePosY: -10,
+      spherePosZ: -10,
+    });
 
-
-    return { renderer, box };
-  }
-
+    function valueChanged(values) {
+      Object.assign(spherePosition, values);
+      boxRotation.value = values.boxRotation;
+    }
+    return { renderer, box, sphere, valueChanged, spherePosition };
+  },
 });
-
-
 </script>
 
 <template>
@@ -36,8 +54,23 @@ export default defineComponent({
     <Scene background="#4DBA87">
       <PointLight :position="{ y: 50, z: 50 }" />
       <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
-        <LambertMaterial />
+        <BasicMaterial>
+          <Texture src="/ink.jpg" refraction :refraction-ratio="0.95" />
+        </BasicMaterial>
       </Box>
+      <Sphere
+        ref="sphere"
+        :position="{
+          x: spherePosition.spherePosX,
+          y: spherePosition.spherePosY,
+          z: spherePosition.spherePosZ,
+        }"
+        :radius="10"
+      >
+        <BasicMaterial>
+          <Texture src="/splash-bg.jpg" refraction :refraction-ratio="0.95" />
+        </BasicMaterial>
+      </Sphere>
     </Scene>
   </Renderer>
 </template>
