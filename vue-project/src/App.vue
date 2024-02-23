5dd57f0de299c304c1f25d1fa5a0dd0207c12bc4
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-02-23 09:03:18 +0100

    page transition

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,18 +3,23 @@ import { RouterView } from "vue-router";
 
 import Footer from "../src/components/Footer.vue";
 import Header from "../src/components/Header.vue";
-
 </script>
 
 <template>
   <div class="app-wrapper dark:bg-white dark:text-black">
     <Header></Header>
-    <RouterView />
+
+    <RouterView v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </RouterView>
+
     <Footer></Footer>
   </div>
 </template>
 
-<style scoped>
+<style> 
 .app-wrapper {
   width: 100%;
   height: 100%;
@@ -23,4 +28,23 @@ import Header from "../src/components/Header.vue";
   padding-top: 2rem;
 }
 
+/* route transition  */
+
+.route-enter-from {
+opacity: 0;
+transform: translateX(100px )
+}
+
+.route-enter-active {
+transition: all 0.5s ease-out;
+}
+
+.route-leave-to {
+  opacity: 1;
+transform: translateX(-100px )
+}
+
+.route-leave-active {
+transition: all 0.5s ease-in;
+}
 </style>
