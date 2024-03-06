8211dfb98768e578ac8b2131459ee9a76e92a565
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-03-06 11:34:19 +0100

    try

--- vue-project/src/components/map/Map.vue
+++ vue-project/src/components/map/Map.vue
@@ -4,10 +4,11 @@
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
       <l-marker-cluster-group v-if="!props.event">
         <template v-for="event in props.events" :key="event.id">
+          // @ts-ignore
           <l-marker
             v-if="event.place.location?.latitude && event.place.location?.longitude"
             :icon="customMarker"
-            :lat-lng="[Number(event.place.location.latitude), Number(event.place.location.longitude)] as [number, number]"
+            :lat-lng="[Number(event.place.location.latitude), Number(event.place.location.longitude)]"
             @click="$emit('onSelected', event)"
           >
             <l-popup :options="popUpOptions">
@@ -16,7 +17,8 @@
           </l-marker>
         </template>
       </l-marker-cluster-group>
-      <l-marker v-else-if="props.event.place.location?.latitude && props.event.place.location?.longitude" :lat-lng="[Number(props.event.place.location.latitude), Number(props.event.place.location.longitude)] as [number, number]" :icon="customMarker">
+      
+      <l-marker v-else-if="props.event.place.location?.latitude && props.event.place.location?.longitude" :lat-lng="[Number(props.event.place.location.latitude), Number(props.event.place.location.longitude)]" :icon="customMarker">
         <l-popup :options="popUpOptions">
           <MapPopup :event="props.event" />
         </l-popup>
