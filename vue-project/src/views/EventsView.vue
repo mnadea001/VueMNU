1c81e34c3a0ec4328fd44aecf01f2ad7732166d1
mnadea001 <mathilde.nadeau@hotmail.fr>
2024-03-20 19:10:42 +0100

    wip

--- vue-project/src/views/EventsView.vue
+++ vue-project/src/views/EventsView.vue
@@ -1,84 +1,60 @@
 <template>
-  <main class="grid lg:grid-cols-2 sm:grid-cols-1 dark:bg-white">
-    <div class="rounded-lg min-h-screen overflow-hidden">
-      <ErrorAlert v-if="errorMessage" :message="errorMessage" />
-      <EventList :events="events" :page="page" />
-      <Pagination
-        :fetch-next-page="fetchNextPage"
-        :fetch-previous-page="fetchPreviousPage"
-        :current-page="currentPage" />
+  <main class="dark:bg-white">
+    <div class="main-content">
+      <div class="content">
+        <h1 class="text-3xl font-semibold text-center mt-4">EVENTS</h1>
+        <div class="content-img my-4">
+          <p class="text-3xl font-semibold text-center mb-4">
+            List of related events
+          </p>
+          <div class="event-card">
+            <EventCard v-for="event in events" :key="event.id" :event="event" />
+          </div>
+        </div>
+      </div>
     </div>
-<!-- <Map @selected="isSelected" :zoom="3" :events="events" /> -->
   </main>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue'
-import EventList from '@/components/events/EventList.vue'
-import Pagination from '@/components/Pagination.vue'
-import ErrorAlert from '@/components/ErrorAlert.vue'
-import { fetchEvents } from '../api/ticketmasterApi'
-import type { Event } from '@/types/event'
-import type { Page } from '@/types/ page'
-export default defineComponent({
-  name: 'YourComponentName',
-  components: {
-    EventList,
-    Pagination,
-    ErrorAlert
-  },
-  setup() {
-    const eventDetails = ref<Event | null>(null)
-    const events = ref<Event[]>([])
-    const page = ref<Page>({
-      size: 0,
-      totalElements: 0,
-      totalPages: 0,
-      number: 0
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import EventCard from "../components/EventCard.vue";
+interface Event {
+  id: string;
+  name: string;
+  url: string;
+  locale: string;
+  description: string;
+  dates: {
+    timezone: string;
+    start: {
+      localDate: string;
+    };
+  };
+}
+
+const events = ref<Event[]>([]);
+
+const fetchEventData = () => {
+  fetch(
+    "https://app.ticketmaster.com/discovery/v2/events.json?keyword=yoga&source=universe&apikey=4efAJ7EnckUfcbAT82O2UvSHqbUaLyGs"
+  )
+    .then(
+      (res: Response) =>
+        res.json() as Promise<{ _embedded: { events: Event[] } }>
+    )
+    .then((data: { _embedded: { events: Event[] } }) => {
+      events.value = data._embedded.events;
     })
-    const currentPage = ref<number>(1)
-    const errorMessage = ref<string | null>(null)
+    .catch((error: Error) => {
+      console.error("Error fetching events:", error);
+    });
+};
 
-    const fetchEventData = async (pageNumber: number): Promise<void> => {
-      try {
-        const response = await fetchEvents(pageNumber)
-        events.value = response._embedded.events
-        page.value = response.page
-      } catch (error) {
-        errorMessage.value = 'Error fetching events data'
-      }
-    }
-
-    const fetchNextPage = async (): Promise<void> => {
-      const nextPageNumber = currentPage.value + 1
-      await fetchEventData(nextPageNumber)
-      currentPage.value = nextPageNumber
-    }
-
-    const fetchPreviousPage = async (): Promise<void> => {
-      const previousPageNumber = currentPage.value - 1
-      await fetchEventData(previousPageNumber)
-      currentPage.value = previousPageNumber
-    }
-
-    onMounted(async () => {
-      await fetchEventData(currentPage.value)
-    })
-
-    return {
-      eventDetails,
-      events,
-      page,
-      currentPage,
-      errorMessage,
-      fetchNextPage,
-      fetchPreviousPage
-    }
-  }
-})
+onMounted(() => {
+  fetchEventData();
+});
 </script>
-
-
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Anton&display=swap");
@@ -97,9 +73,14 @@ h2 {
   display: flex;
   justify-content: center;
 }
+
 .event-card {
   width: 100%;
 }
+
+
+
+
 .content-img {
   display: flex;
   flex-direction: column;
