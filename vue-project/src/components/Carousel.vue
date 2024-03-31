06cb33f851b2d4dcd7a999022d96e5713515aa05
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-03-31 19:35:59 +0200

    WIP

---
+++ vue-project/src/components/Carousel.vue
@@ -0,0 +1,124 @@
+<template>
+  <Splide :options="{ rewind: true }" aria-label="Vue Splide Example">
+    <SplideSlide>
+      <div class="slide-content">
+        <div class="div-img">
+          <img :src="slide1" alt="Sample 1" />
+        </div>
+        <div class="slide-text">
+          <h2 class="text-2xl font-bold text-center my-4">Ahimsa (Non-violence)</h2>
+          <p class="font-regular text-center my-4">
+            Ahimsa, or non-violence, is one of the fundamental principles of yoga philosophy. It
+            extends beyond refraining from physical harm and includes compassion and kindness toward
+            all living beings. By choosing a vegetarian diet, yogis avoid participating in the
+            suffering and exploitation of animals for food, aligning their actions with the
+            principle of ahimsa.
+          </p>
+        </div>
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="slide-content">
+        <div class="slide-text">
+          <h2 class="text-2xl font-bold text-center my-4">Sattvic Diet for Mental Clarity</h2>
+          <p class="font-regular text-center my-4">
+            Yoga aims to achieve a state of mental clarity and inner peace. According to yogic
+            teachings, different foods have distinct energetic qualities that can influence one's
+            state of mind. A sattvic diet, which is primarily vegetarian and includes fresh fruits,
+            vegetables, grains, nuts, and dairy products, is believed to promote purity, clarity,
+            and harmony within the body and mind. Consuming foods that are light, pure, and easily
+            digestible supports the practice of yoga by fostering a calm and focused state of being.
+          </p>
+        </div>
+        <div class="div-img">
+          <img :src="slide2" alt="Sample 1" />
+        </div>
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="slide-content">
+        <div class="div-img">
+            <img :src="slide3" alt="Sample 1" />
+        </div>
+        <div class="slide-text">
+          <h2 class="text-2xl font-bold text-center my-4">Enhanced Pranic Energy</h2>
+          <p class="font-regular text-center my-4">
+            Yogic philosophy recognizes the existence of prana, the life force energy that sustains
+            all living beings. Vegetarian foods are believed to be rich in prana, making them
+            conducive to maintaining and enhancing the flow of vital energy within the body. By
+            consuming plant-based foods that are vibrant and full of life force energy, yogis aim to
+            nourish not only their physical bodies but also their subtle energy systems, supporting
+            overall health and vitality.
+          </p>
+        </div>
+      </div>
+    </SplideSlide>
+    <SplideSlide>
+      <div class="slide-content">
+        <div class="slide-text">
+          <h2 class="text-2xl font-bold text-center my-4">
+            Environmental and Ethical Considerations
+          </h2>
+          <p class="font-regular text-center my-4">
+            Beyond personal health benefits, vegetarianism reflects an awareness of the
+            interconnectedness of all living beings and a commitment to environmental
+            sustainability. The livestock industry is a significant contributor to environmental
+            degradation, including deforestation, water pollution, and greenhouse gas emissions. By
+            choosing plant-based foods, yogis minimize their ecological footprint and contribute to
+            a more compassionate and sustainable world.
+          </p>
+        </div>
+        <div class="div-img">
+        <img :src="slide4" alt="Sample 1" /></div>
+      </div>
+    </SplideSlide>
+  </Splide>
+</template>
+
+<script setup lang="ts">
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import { defineComponent } from 'vue'
+import '@splidejs/vue-splide/css'
+import slide1 from '../assets/images/slide1.jpg'
+import slide2 from '../assets/images/slide2.jpg'
+import slide3 from '../assets/images/slide3.jpg'
+import slide4 from '../assets/images/slide4.jpg'
+
+defineComponent({
+  components: { Splide, SplideSlide }
+})
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Caprasimo&display=swap');
+
+h2 {
+  font-family: 'Caprasimo', sans-serif;
+}
+.slide-content {
+  display: flex;
+  margin: 50px;
+}
+.slide-text {
+  width: 50%;
+  margin: 50px;
+}
+.div-img {
+  width: 50%;
+  margin: 50px;
+}
+img {
+  max-width: 500px;
+  max-height: 400px;
+}
+
+@media screen and (max-width: 768px) {
+    .slide-content {
+  display: flex;
+flex-direction: column;
+}
+
+.div-img {
+display: none;
+}
+}
+</style>
--- vue-project/src/views/FoodView.vue
+++ vue-project/src/views/FoodView.vue
@@ -1,157 +1,95 @@
 <template>
   <main>
     <div class="title-container">
-      <button
-        @click="goBack"
-        class="text-lg font-medium text-black-500 hover:underline mb-4 flex"
-      >
+      <button @click="goBack" class="text-lg font-medium text-black-500 hover:underline mb-4 flex">
         <img :src="arrowIcon" class="w-10 h-10" /> Back
       </button>
       <h1 class="text-3xl text-center my-4">Veggie is good !!!</h1>
-      <h4 class="text-xl font-semibold text-center my-4">
-        Vegetarianism holds a significant place in the lifestyle and philosophy
-        of yoga, serving as more than just a dietary choice but rather a
-        cornerstone of spiritual and holistic living. Yogis, practitioners of
-        yoga, often adhere to a vegetarian diet for various reasons, all of
+      <h4 class="text-xl text-center my-4">
+        Vegetarianism holds a significant place in the lifestyle and philosophy of yoga, serving as
+        more than just a dietary choice but rather a cornerstone of spiritual and holistic living.
+        Yogis, practitioners of yoga, often adhere to a vegetarian diet for various reasons, all of
         which align with the core principles of yoga practice.
       </h4>
     </div>
-    <p class="my-4 ml-5">Scroll vertically to see all meals:</p>
-    <div class="container-card my-4">
-      <RouterLink
-        v-for="(meal, index) in meals"
-        :key="index"
-        :to="`/meal/${meal.idMeal}`"
-      >
+    <p class="my-4 ml-5 scroll-text">Scroll vertically to see all meals:</p>
+    <div class="container-card my-4 pb-5">
+      <RouterLink v-for="(meal, index) in meals" :key="index" :to="`/meal/${meal.idMeal}`">
         <MealCard :img="meal.strMealThumb" :strMeal="meal.strMeal" />
       </RouterLink>
     </div>
-    <div class="title-container">
-      <h2 class="text-2xl font-bold text-center my-4">Ahimsa (Non-violence)</h2>
-      <h4 class="text-xl font-regular text-center my-4">
-        Ahimsa, or non-violence, is one of the fundamental principles of yoga
-        philosophy. It extends beyond refraining from physical harm and includes
-        compassion and kindness toward all living beings. By choosing a
-        vegetarian diet, yogis avoid participating in the suffering and
-        exploitation of animals for food, aligning their actions with the
-        principle of ahimsa.
-      </h4>
-    </div>
-    <div class="title-container">
-      <h2 class="text-2xl font-bold text-center my-4">
-        Sattvic Diet for Mental Clarity
-      </h2>
-      <h4 class="text-xl font-regular text-center my-4">
-        Yoga aims to achieve a state of mental clarity and inner peace.
-        According to yogic teachings, different foods have distinct energetic
-        qualities that can influence one's state of mind. A sattvic diet, which
-        is primarily vegetarian and includes fresh fruits, vegetables, grains,
-        nuts, and dairy products, is believed to promote purity, clarity, and
-        harmony within the body and mind. Consuming foods that are light, pure,
-        and easily digestible supports the practice of yoga by fostering a calm
-        and focused state of being.
-      </h4>
-    </div>
-    <div class="title-container">
-      <h2 class="text-2xl font-bold text-center my-4">
-        Enhanced Pranic Energy
-      </h2>
-      <h4 class="text-xl font-regular text-center my-4">
-        Yogic philosophy recognizes the existence of prana, the life force
-        energy that sustains all living beings. Vegetarian foods are believed to
-        be rich in prana, making them conducive to maintaining and enhancing the
-        flow of vital energy within the body. By consuming plant-based foods
-        that are vibrant and full of life force energy, yogis aim to nourish not
-        only their physical bodies but also their subtle energy systems,
-        supporting overall health and vitality.
-      </h4>
-    </div>
-    <div class="title-container">
-      <h2 class="text-2xl font-bold text-center my-4">
-        Environmental and Ethical Considerations
-      </h2>
-      <h4 class="text-xl font-regular text-center my-4">
-        Beyond personal health benefits, vegetarianism reflects an awareness of
-        the interconnectedness of all living beings and a commitment to
-        environmental sustainability. The livestock industry is a significant
-        contributor to environmental degradation, including deforestation, water
-        pollution, and greenhouse gas emissions. By choosing plant-based foods,
-        yogis minimize their ecological footprint and contribute to a more
-        compassionate and sustainable world.
-      </h4>
-      <br />
-      <hr />
-      <br />
-      <br />
-      <h2 class="text-xl font-semibold text-center mt-4">
-        In conclusion, vegetarian food holds immense importance for yogis,
-        serving as a practical manifestation of yogic principles such as ahimsa,
-        promoting mental clarity and inner peace, enhancing pranic energy, and
-        aligning with ethical and environmental values. By embracing a
-        vegetarian diet, yogis not only nourish their bodies but also cultivate
-        compassion, mindfulness, and harmony in their lives and the world around
-        them.
-      </h2>
-    </div>
+<h2 class="text-3xl text-center mt-4 pt-5">4 main reasons for vegetarism:</h2>
+    <Carousel />
+<div class="conclusion-food">
+    <h3 class="text-xl text-center mb-4">
+      In conclusion, vegetarian food holds immense importance for yogis, serving as a practical
+      manifestation of yogic principles such as ahimsa, promoting mental clarity and inner peace,
+      enhancing pranic energy, and aligning with ethical and environmental values. By embracing a
+      vegetarian diet, yogis not only nourish their bodies but also cultivate compassion,
+      mindfulness, and harmony in their lives and the world around them.
+    </h3>
+  </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, computed } from "vue";
-import { RouterLink, useRouter } from "vue-router";
-import MealCard from "../components/MealCard.vue";
-import backIcon from "../assets/back.svg";
-import whiteBack from "../assets/white-back.png";
-import { useDark } from "@vueuse/core";
-
-const isDark = useDark();
+import { onMounted, ref, computed } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+import MealCard from '../components/MealCard.vue'
+import backIcon from '../assets/back.svg'
+import whiteBack from '../assets/white-back.png'
+import { useDark } from '@vueuse/core'
+import Carousel from '@/components/Carousel.vue'
+const isDark = useDark()
 const arrowIcon = computed(() => {
-  return isDark.value ? backIcon : whiteBack;
-});
+  return isDark.value ? backIcon : whiteBack
+})
 interface Meal {
-  idMeal: string;
-  strMeal: string;
-  strMealThumb: string;
+  idMeal: string
+  strMeal: string
+  strMealThumb: string
 }
 
-const router = useRouter();
+const router = useRouter()
 
 function goBack() {
-  router.go(-1);
+  router.go(-1)
 }
 
-const meals = ref<Meal[]>([]);
+const meals = ref<Meal[]>([])
 
 const fetchMealData = () => {
-  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
+  fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian')
     .then((res: Response) => res.json() as Promise<{ meals: Meal[] }>)
     .then((data: { meals: Meal[] }) => {
-      console.log(data);
-      meals.value = data.meals;
+      console.log(data)
+      meals.value = data.meals
     })
     .catch((error: Error) => {
-  
-      console.error("Error fetching meals:", error);
-
-    });
-};
+      console.error('Error fetching meals:', error)
+    })
+}
 
 onMounted(() => {
-  fetchMealData();
-});
+  fetchMealData()
+})
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Anton&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Caprasimo&display=swap');
 
 main {
   width: 100vw;
   height: 100%;
 }
 h1 {
-  font-family: "Anton", sans-serif;
+  font-family: 'Anton', sans-serif;
 }
 
+h2, .scroll-text {
+font-family: 'Caprasimo', sans-serif;
+}
 .title-container {
   margin: 120px;
 }
@@ -163,5 +101,9 @@ h1 {
   white-space: nowrap;
   object-fit: contain;
   scroll-snap-type: x mandatory;
+}
+
+.conclusion-food {
+margin: 100px;
 }
 </style>
