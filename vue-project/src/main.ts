a48633071e6fa3a36bcae5bdafcb2f43370332e8
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-03-31 19:54:57 +0200

    fix ?

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,5 +1,6 @@
 import './assets/main.css'
 import './index.css'
+import VueSplide from '@splidejs/vue-splide';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { MotionPlugin } from '@vueuse/motion'
@@ -10,5 +11,6 @@ import router from './router'
 const app = createApp(App)
 app.use(MotionPlugin)
 app.use(createPinia())
+app.use( VueSplide );
 app.use(router)
 app.mount('#app')
