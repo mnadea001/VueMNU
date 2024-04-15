4341121f5752cbced93b3e85d23e9f756e1e21fe
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-15 23:11:15 +0200

    fix ts ?8

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -35,9 +35,9 @@
   
   </div>
 </template>
-<script lang="ts">
+<script>
 import StartButton from '@/components/StartButton.vue'
-import { ref, onMounted, onUnmounted, Ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import VTypical from 'vue-typical'
 
 interface ElementRefs {
