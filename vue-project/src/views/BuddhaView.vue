cf4491b6246d16bcb2e08640810e03f3345ab330
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-15 22:48:31 +0200

    clean buddha

--- vue-project/src/views/BuddhaView.vue
+++ vue-project/src/views/BuddhaView.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 import { defineComponent } from 'vue'
 import { Renderer, Scene, Camera, PointLight, AmbientLight, GltfModel } from 'troisjs'
-import { useRouter } from 'vue-router'
-import whiteBack from '../assets/white-back.png'
-import { useDark } from '@vueuse/core'
-import { computed } from 'vue'
-import backIcon from '../assets/back.svg'
 import mouseScroll from '../assets/mouseScroll.png'
 import aroundImg from '../assets/aroundImg.svg'
+import BackButton from '@/components/BackButton.vue'
 
-const isDark = useDark()
-const arrowIcon = computed(() => {
-  return isDark.value ? backIcon : whiteBack
-})
+
 defineComponent({
   components: {
     Renderer,
@@ -24,55 +17,44 @@ defineComponent({
   }
 })
 
-const router = useRouter()
 
-function goBack() {
-  router.go(-1)
-}
 </script>
 
 <template>
   <main>
-  <div class="content-btn">
-    <button @click="goBack" class="text-white-500 dark:text-black hover:underline mb-4 flex">
-      <img :src="arrowIcon" class="w-10 h-10 dark:color-white" /> back
-    </button>
-  </div>
-  <div class="box-docker">
-    <div class="first-box">
-      <h2 class="text-center text-black me-5">scroll down the black box to see buddha</h2>
-      <img :src="mouseScroll" class="w-10 h-10 ms-5 me-5" alt="" />
+    <BackButton />
+    <div class="box-docker">
+      <div class="first-box">
+        <h2 class="text-center text-black me-5">scroll down the black box to see buddha</h2>
+        <img :src="mouseScroll" class="w-10 h-10 ms-5 me-5" alt="" />
+      </div>
+      <div class="second-box">
+        <h2 class="text-center text-black ms-5 me-5">then you can drag around buddha</h2>
+        <img :src="aroundImg" class="w-10 h-10 ms-5 dark:color-white" alt="" />
+      </div>
     </div>
-    <div class="second-box">
-      <h2 class="text-center text-black ms-5 me-5">then you can drag around buddha</h2>
-      <img :src="aroundImg" class="w-10 h-10 ms-5 dark:color-white" alt="" />
+    <div class="audio-box">
+      <p class="text-center text-black me-5">start mantra</p>
+      <audio controls class="audio">
+        <source src="../assets/music/om.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
-  </div>
-  <div class="audio-box">
-    <p class="text-center text-black me-5">start mantra</p>
-    <audio controls class="audio">
-      <source src="../assets/music/om.mp3" type="audio/mpeg" />
-      Your browser does not support the audio element.
-    </audio>
-  </div>
 
-  <Renderer ref="renderer" antialias orbit-ctrl resize="window">
-    <Camera :position="{ x: 0, z: 20 }" />
-    <Scene>
-      <AmbientLight />
-      <PointLight color="white" :position="{ x: 100, y: 1000, z: 40 }" :intensity="1" />
-      <GltfModel src="/models/buddha/buddha.gltf" />
-    </Scene>
-  </Renderer>
-  <br />
-</main>
+    <Renderer ref="renderer" antialias orbit-ctrl resize="window">
+      <Camera :position="{ x: 0, z: 20 }" />
+      <Scene>
+        <AmbientLight />
+        <PointLight color="white" :position="{ x: 100, y: 1000, z: 40 }" :intensity="1" />
+        <GltfModel src="/models/buddha/buddha.gltf" />
+      </Scene>
+    </Renderer>
+    <br />
+  </main>
 </template>
 <style scoped>
 main {
   width: 100vw;
-}
-.content-btn {
-  margin-left: 80px;
 }
 .audio-box {
   display: flex;
